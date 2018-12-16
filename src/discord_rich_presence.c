c03ffb52ff5b38fb1f36f50f050f792f90816e0a
Gegel85 <Gegel85@laposte.net>
2018-12-16 12:25:35 +0100

    Displayed game mode in rich presence

--- src/discord_rich_presence.c
+++ src/discord_rich_presence.c
@@ -2,6 +2,8 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <string.h>
+#include <stdio.h>
+#include "globals.h"
 
 void	initDiscordRichPresence(void)
 {
@@ -14,10 +16,15 @@ void	initDiscordRichPresence(void)
 void	updateDiscordPresence(char const *details, char const *state, int timestamp, bool end, char const *large_image, char const *large_text)
 {
 	DiscordRichPresence	discordPresence;
+	char			buffer[strlen(details) + 25];
 
+	memset(buffer, 0, sizeof(buffer));
+	if (details) {
+		sprintf(buffer, "%s (Game mode: %s)", details, game.grid.jumpingMines ? "Jumping mines" : "Normal");
+	}
 	memset(&discordPresence, 0, sizeof(discordPresence));
 	discordPresence.state = state;
-	discordPresence.details = details;
+	discordPresence.details = buffer;
 	if (end)
 		discordPresence.endTimestamp = timestamp;
 	else
